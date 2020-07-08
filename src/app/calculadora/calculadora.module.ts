8c48920752b3ea8784decdbcf80baf3dcd2b2cc8
Natalia Uyu <nasilva.java@gmail.com>
2020-07-08 14:29:35 -0300

    Criado serviço, alterando component, começando HTML da calculadora

--- src/app/calculadora/calculadora.module.ts
+++ src/app/calculadora/calculadora.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalculadoraComponent } from './components';
-
+import { CalculadoraService } from './services';
 
 
 @NgModule({
@@ -11,6 +11,9 @@ import { CalculadoraComponent } from './components';
   ],
   exports: [
   	CalculadoraComponent
+  ],
+  providers: [
+    CalculadoraService
   ]
 })
 export class CalculadoraModule { }
